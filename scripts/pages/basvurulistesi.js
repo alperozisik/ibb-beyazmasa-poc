f51169b54d230a4a7470afe92595f93bf47f42a6
alperozisik <smartface9@users.smarfacecloud.io>
2016-10-11 12:47:51 +0000

    added rbox fill

--- scripts/pages/basvurulistesi.js
+++ scripts/pages/basvurulistesi.js
@@ -19,32 +19,64 @@
         }
     }
 
+    var oReq = new XMLHttpRequest();
+    oReq.onload = reqListener;
+    oReq.setRequestHeader("Accept", "application/json");
+    oReq.setRequestHeader("Authorization", "Basic MTk2MDA0MTg2NzI6MjM3NTY3ODkw");
+    oReq.open("GET", "https://osb-mwwebgate.ibb.gov.tr/External/IBBMobilBeyazmasaWebServisleri/IBBMobilAnonimBasvuruSorgula/ProxyService/IBBMobilAnonimBasvuruSorgulaRESTPS/IBBMobilServiceRequestQuery?contactId=1-QKBL1W&password=237567890&tckn=19600418672", true);
     /**
      * Creates action(s) that are run when the page is appeared
      * @param {EventArguments} e Returns some attributes about the specified functions
      * @this Pages.basvurulistesi
      */
     function basvurulistesi_onShow() {
-        function reqListener() {
-            alert(oReq.responseText);
-        }
 
-        var oReq = new XMLHttpRequest();
-        oReq.onload  = reqListener;
-        oReq.setRequestHeader("Accept", "application/json");
-        oReq.setRequestHeader("Authorization", "Basic MTk2MDA0MTg2NzI6MjM3NTY3ODkw");
-        oReq.setRequestHeader("Content-Type", "application/json");
-        oReq.open("GET", "https://osb-mwwebgate.ibb.gov.tr/External/IBBMobilBeyazmasaWebServisleri/IBBMobilAnonimBasvuruSorgula/ProxyService/IBBMobilAnonimBasvuruSorgulaRESTPS/IBBMobilServiceRequestQuery?contactId=1-QKBL1W&password=237567890&tckn=19600418672", true);
+
+
         oReq.send();
     }
-    
+
     var btnBack = new SMF.UI.TextButton({
-        text:"back",
+        text: "back",
         onPressed: function(e) {
             Pages.back();
-        }
+        },
+        top: 0,
+        left: 0,
+        height: "10%",
+        width: "100%"
     });
     basvurulistesi.add(btnBack);
 
+    var rbBlist = new SMF.UI.RepeatBox({
+        top: "10%",
+        height: "90%",
+        width: "100%",
+        left: 0,
+        onRowRender: rowRender
+    });
+    rbBlist.itemTemplate.height = "40dp";
+    
+    var lblBasvuruNo = new SMF.UI.Label({
+        left: "8dp",
+        height: "15dp",
+        top: "8dp",
+        width: "100%",
+        touchEnabled: false
+    });
+    rbBlist.itemTemplate.add(lblBasvuruNo);
+
+
+
+    function reqListener() {
+        var response = JSON.parse(oReq.responseText);
+        rbBlist.dataSource = response.ibbMobilServiceRequest;
+        rbBlist.refresh();
+    }
+    
+    function rowRender(e) {
+        var dataItem = rbBlist.dataSource[e.rowIndex];
+        lblBasvuruNo.text = dataItem.sRNumber;
+    }
 
 })();
