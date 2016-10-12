9feb55f5371c1d73bd0b13d8650e7bc3a6047931
alperozisik <smartface9@users.smarfacecloud.io>
2016-10-12 10:11:18 +0000

    fixed update check

--- scripts/app.js
+++ scripts/app.js
@@ -14,41 +14,6 @@ function Application_OnStart(e) {
 	//include("pages/index.js");
 
 	Pages.mainpage.show();
-
-
-	//Checks if there is a valid update. If yes returns result object.    
-	Application.checkUpdate(function(err, result) {
-		if (err) {
-			//Checking for update is failed
-			alert("check update error: " + err);
-		}
-		else {
-			//There is a valid update. We can show the meta info to inform our app user.
-			alert(JSON.stringify(result.meta));
-			//There is an update waiting to be downloaded. Let's download it.
-			result.download(function(err, result) {
-				if (err) {
-					//Download failed
-					alert("download error: " + err);
-				}
-				else {
-
-					//All files are received, we'll trigger an update.
-					result.updateAll(function(err) {
-						if (err) {
-							//Updating the app with downloaded files failed
-							alert("update all error: " + err);
-						}
-						else {
-
-							//After that the app will be restarted automatically to apply the new updates
-							Application.restart();
-						}
-					});
-				}
-			});
-		}
-	});
 }
 
 function Application_OnError(e) {
